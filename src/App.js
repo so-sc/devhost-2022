56b3490245fa1e1e6828d8b8a2144f23157fc674
fauwara <fawazabid96@gmail.com>
2022-05-21 18:32:24 +0530

    initialized repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Nav from './components/Nav/Nav.js'
+import Container from './components/Container/Container.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Container>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
