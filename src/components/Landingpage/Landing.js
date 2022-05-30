91bbdcb535375ca14f24dd180ba791b2793595fb
fauwara <fawazabid96@gmail.com>
2022-05-30 15:36:31 +0530

    made slightly more responsive

--- src/components/Landingpage/Landing.js
+++ src/components/Landingpage/Landing.js
@@ -27,6 +27,7 @@ function Landingpage() {
                   </p>
                   <div className='btns'>
                     <button className='landing-button'>Register Now</button>
+                    <button className='landing-button red-button'>Register Now</button>
                   </div>
                 </div>
               </div>
@@ -40,22 +41,19 @@ function Landingpage() {
             <div className='event-info'>
               <h2>Insightful Talks</h2>
               <p>
-                lorem ipsum lesgo bro fake lorem ipsum lmao kknow
-                will get to know about this lol, hi dhanush
+              Hear what the experts have to share about a wide range of concepts and technologies. Know more about the recent trends, initiatives, innovations and much more. Expand your knowledge and stand a chance to interact with experts from various technical fields.
               </p>
             </div>
             <div className='event-info ew'>
               <h2>Exciting Workshops</h2>
               <p>
-                lorem ipsum lesgo bro fake lorem ipsum lmao kknow
-                will get to know about this lol, hi dhanush
+              There’s no better way to master a technical skill than to actually get coding. Come join us in a series of talks and workshop exploring various domains. The talks and workshop involve hands-on learning along with discussion and interaction on a given topic.
               </p>
             </div>
             <div className='event-info'>
               <h2>Fun Icebreakers</h2>
               <p>
-                lorem ipsum lesgo bro fake lorem ipsum lmao kknow
-                will get to know about this lol, hi dhanush
+              A virtual trivia could be just what your social (distancing) life needs. Stand a chance to win exciting prizes by answering questions that test your knowledge on subjects both practical and ridiculously random topics!
               </p>
             </div>
           </div>
