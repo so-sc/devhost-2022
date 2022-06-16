c4d395da8da84094122a974ea825a727fe57e04b
carbonxx <vishnunv142000@gmail.com>
2022-06-16 15:07:00 +0530

    fixed some css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,23 @@
-import '../../styles/variables.css'
-import aboutImg from '../../assets/about.png'
-import './About.css'
+import "../../styles/variables.css";
+import aboutImg from "../../assets/about.png";
+import "./About.css";
 
 function About() {
   return (
-    <div className='AboutBody'>
-      <img className='About-img' alt='img-abt' src={aboutImg}/>
-      <div className='About-desc'>
-        <h1 className='About-heading'>
-          About
-        </h1>
-        <div className='About-content'>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic</p>
+    <div className="AboutBody">
+      <img className="About-img" alt="img-abt" src={aboutImg} />
+      <div className="About-desc">
+        <h1 className="About-heading">About</h1>
+        <div className="About-content">
+          <p>
+            devhost is the annual flagship event of SOSC. Our prime goal is to
+            bring young and skilled student developers from anywhere across the
+            globe under the same umbrella. This event aims to create a platform
+            to collaborate with various clubs and organizations, discuss the
+            newest technologies and trends in the IT industry an also train
+            students for the same. devhost:22 claims to be a tech extravaganza,
+            and the biggest event under SOSC so far.
+          </p>
         </div>
       </div>
     </div>
@@ -21,7 +27,7 @@ function About() {
 function AboutBody() {
   return (
     // <div>
-      <About />
+    <About />
     // </div>
   );
 }
