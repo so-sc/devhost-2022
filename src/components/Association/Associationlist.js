54abf8f52edd2431f0770fb96a2f9f9360844a43
alpha-oss <monish.shetty770@gmail.com>
2022-06-19 21:27:54 +0530

    init

--- src/components/Association/Associationlist.js
+++ src/components/Association/Associationlist.js
@@ -4,31 +4,11 @@ export const associationLists = [
         img: require('../../assets/associations/campus.png')
     },
     {
-        name:'codechef',
-        img: require('../../assets/associations/ccscem.png')
-    },
-    {
         name:'django',
         img: require('../../assets/associations/django.png')
     },
     {
-        name:'googledsc',
-        img: require('../../assets/associations/dsc.png')
-    },
-    {
-        name: 'girlswhocode',
-        img: require('../../assets/associations/gwc.png')
-    },
-    {
         name:'hackclub',
         img: require('../../assets/associations/hack.png')
-    },
-    {
-        name: 'microsoft',
-        img: require('../../assets/associations/mlsa.png')
-    },
-    {
-        name: 'nodeschool',
-        img: require('../../assets/associations/node.png')
     },
 ]
