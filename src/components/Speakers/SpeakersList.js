39d5e3794cd37879b0829fced9f22cfabff6abdf
Pavan <pavannaik2205@gmail.com>
2022-07-10 12:26:46 +0530

    updated events and speaker content

--- src/components/Speakers/SpeakersList.js
+++ src/components/Speakers/SpeakersList.js
@@ -1,33 +1,27 @@
 export const speakersList = [
     {
         name: 'Jehad Kun',
-        role: 'UI/UX Designer at MobStac',
-        img: require('../../assets/speakers/jehad.png'),
-        desc: 'Jehad Mohamed is a Computer Science graduate who has been dabbling with computer vision and the vast domain of design for the past few years and has loved every bit of it. His design journey includes interning at Canva and free lancing for multiple renowned companies, to name a few. He wants to help build tools that allow non-designers to create beautiful designs.',
+        role: 'Lead Product Designer at Beaconstac HQ',
+		img: require('../../assets/speakers/jehad.png'),
+        desc:"Jehad Mohamed is a Computer Science graduate who has been dabbling with computer vision and the vast domain of design for the past few years and has loved every bit of it. His design journey includes interning at Canva and free lancing for multiple renowned companies, to name a few.  He wants to help build tools that allow non-designers to create beautiful designs.",
     },
     {
-        name: 'Madhu Akula',
-        role: 'Creator of Kubernetes Goat',
-        img: require('../../assets/speakers/madhu-akula.jpg'),
-        desc: 'Madhu Akula is the creator of Kubernetes Goat. He is also a published author and cloud native security researcher. He is an active member of international security, devops and cloud native communities. He frequently speaks and runs training sessions at security events and conferences around the world including DEFCON, BlackHat USA, USENIX LISA, O’Reilly Velocity, GitHub Satellite event, Appsec EU, All Day DevOps, DevSecCon and multiple others. He is also the co-author of Security Automation with Ansible2 which is listed as a technical resource by Red Hat Ansible.',
+        name: 'Swasthik Shetty',
+        role: 'Incoming Cloud solutions engineer at Google',
+		img: require('../../assets/speakers/jehad.png'),
+        desc:"Swasthik Shetty is a final year Information Science student who is a  brilliant developer and designer. His passion towards coding , dependable leadership and initiative led to him being the president at SOSC. Being the current President of SOSC, he has undertaken many projects and events to great success. He has worked under the Crime Investigation Department and has also freelanced for multiple companies. He is even an Incoming Technical Solutions Engineer at Google."
     },
     {
-        name: 'Siddhant Agarwal',
-        role: 'Program Coordinator at Google India',
-        img: require('../../assets/speakers/sidhant-agarwal.jpg'),
-        desc: 'Siddhant was a program coordinator at Google,India. He is also one of ACM’s distinguished speakers and an Intel SoftwareInnovator. Being an avid public speaker, he has spoken at more than 200+ national and international forums impacting over 50K individuals in the last 4-5 years. He loves working with startups and helping them scale in User Experience and improve their designs.',
+        name: 'Abhishek P',
+        role: 'Software engineer at Velotio',
+		img: require('../../assets/speakers/jehad.png'),
+        desc:"Abhishek P is a skilled software engineer with a strong background in Python, backend engineering, data engineering, and network security. He has broad expertise in a variety of fields, including networking, distributed systems, legal technology, and finance. At the moment, he finds the block chain and its many applications extremely interesting."
     },
     {
-        name: 'Sam Poder',
-        role: 'Hackclub Lead, Founder of the Singapore STEM Club',
-        img: require('../../assets/speakers/sam-poder.jpg'),
-        desc: 'Sam, a high schooler in Singapore, finds joy in building ✨ jazzy 🌈 media experiences for people to enjoy. He is a massive fan of open source and collaborating with others to make things. In his community, he works hard to introduce students from all backgrounds to technology and support them.',
-    },
-    {
-        name: 'Siddharth Dayalwal',
-        role: 'Community Development Intern at SAWO labs',
-        img: require('../../assets/speakers/siddharth-dayalwal.jpg'),
-        desc: 'Siddharth Dayalwal is a Community Development Intern at SAWO labs,GitHub Campus Expert ,Microsoft Learn Student Ambassador, AngelHack Student Ambassador,Coding Blocks ~ HackerEarth Ambassador and Former Progate Community Intern.',
+        name: 'Prakhyath Rai',
+        role: ' Senior Advisory technical services specialist at IBM',
+		img: require('../../assets/speakers/jehad.png'),
+        desc:"Prakhyath Rai is a Senior Advisory Technical Services Specialist and a Data Scientist at IBM. He has done his Phd in Text Analytics. With passion and dedication for teaching, He's been assisting the students excel at programming since he was 18. He is a proud alumni of Sahyadri and has also worked as an Assistant Professor at Sahyadri college. He has previously given various sessions on Problem solving and programming."
     },
 
 ]
